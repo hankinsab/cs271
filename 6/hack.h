c12859f4899f1646b2c7db822c2aae3a72c913e1
Abbey Hankins <hankinab@oregonstate.edu>
2024-12-05 15:47:15 -0800

    debugging

--- 6/hack.h
+++ 6/hack.h
@@ -17,22 +17,22 @@
 #define MAX_PREDEFINED_SYMBOL_LENGTH 10
 
 #define OPCODE_TO_BINARY(opcode) \
-  (opcode & 0x8000 ? '1' : '0'), \
-  (opcode & 0x4000 ? '1' : '0'), \
-  (opcode & 0x2000 ? '1' : '0'), \
-  (opcode & 0x1000 ? '1' : '0'), \
-  (opcode & 0x0800 ? '1' : '0'), \
-  (opcode & 0x0400 ? '1' : '0'), \
-  (opcode & 0x0200 ? '1' : '0'), \
-  (opcode & 0x0100 ? '1' : '0'), \
-  (opcode & 0x0080 ? '1' : '0'), \
-  (opcode & 0x0040 ? '1' : '0'), \
-  (opcode & 0x0020 ? '1' : '0'), \
-  (opcode & 0x0010 ? '1' : '0'), \
-  (opcode & 0x0008 ? '1' : '0'), \
-  (opcode & 0x0004 ? '1' : '0'), \
-  (opcode & 0x0002 ? '1' : '0'), \
-  (opcode & 0x0001 ? '1' : '0')
+    (opcode & 0x8000 ? '1' : '0'), \
+    (opcode & 0x4000 ? '1' : '0'), \
+    (opcode & 0x2000 ? '1' : '0'), \
+    (opcode & 0x1000 ? '1' : '0'), \
+    (opcode & 0x0800 ? '1' : '0'), \
+    (opcode & 0x0400 ? '1' : '0'), \
+    (opcode & 0x0200 ? '1' : '0'), \
+    (opcode & 0x0100 ? '1' : '0'), \
+    (opcode & 0x0080 ? '1' : '0'), \
+    (opcode & 0x0040 ? '1' : '0'), \
+    (opcode & 0x0020 ? '1' : '0'), \
+    (opcode & 0x0010 ? '1' : '0'), \
+    (opcode & 0x0008 ? '1' : '0'), \
+    (opcode & 0x0004 ? '1' : '0'), \
+    (opcode & 0x0002 ? '1' : '0'), \
+    (opcode & 0x0001 ? '1' : '0')
 
 enum symbol_id{
 
